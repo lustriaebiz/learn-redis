ccfb0ca809986145fa66400b37be631583b12bc3
lustria ebiz <lustriaebiz@gmail.com>
2020-03-05 17:31:26 +0700

    done

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,47 @@
-import axios     from 'axios';
+import express  from'express';
+import request  from 'superagent';
+import redis    from 'redis';
 
-require('dotenv').config();
-const url = String(process.env.URL);
+const client    = redis.createClient();
+const app       = express();
+const key       = 'data';
 
 class Main {
 
-    run() {
+    run(port:number) {
 
-        axios(url).then(response => {
-            
+        app.get('/users', this.cache, function(req:any, res:any){
 
-        }).catch(error => {
-            console.log('Error: ', error);
+            request.get(`https://jsonplaceholder.typicode.com/todos`, function (err:any, response:any) {
+                if (err) throw err;
+    
+                let data = response.body;
+
+                client.setex(key, 3600, JSON.stringify(data));
+    
+                res.send(data);
+            });
             
+        });
+
+        app.listen(port, function () {
+            console.log(`listening on port ${port}`)
         });
         
     }
 
+    cache(req:any, res:any, next:any) {
+        client.get(key, function (err:any, data:any) {
+            if (err) throw err;
+    
+            if (data != null) {
+                res.send(data);
+            } else {
+                next();
+            }
+        });
+    }
 }
 
-let main = new Main;
-
-main.run();
-
-
-
+let main$ = new Main;
+main$.run(3000);
